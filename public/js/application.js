458012233aff4f10cec6f5497abd587230eb5a8c
Ilamaran Gurunathan <ilamaran.g@gmail.com>
2017-08-09 15:08:38 -0400

    Added feature to fade the article after scraping

--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
   var renderArticles = function() {
     $.ajax({
       method: "GET",
@@ -12,7 +13,7 @@ $(document).ready(function(){
       var cardDeck = $('<div class="card-deck">').appendTo(cardRow)
       doc.forEach(function(article,index){
         console.log(article);
-        var newCardColumnDiv = $('<div class="col-md-4">').appendTo(cardDeck);
+        var newCardColumnDiv = $('<div class="col-md-4" id= card' + index+ '>').appendTo(cardDeck);
         var newCardClassDiv = $('<div class="card card-inverse card-primary text-center">').appendTo(newCardColumnDiv);
         var newCardBlockDiv = $('<div class="card-block">').appendTo(newCardClassDiv);
         var newCardTitle = $('<h4 class="card-title"> This is Article #' + (index + 1) + '</h4>').appendTo(newCardBlockDiv);
@@ -22,12 +23,41 @@ $(document).ready(function(){
         $(newArticleLink).attr('href',article.link);
 
         var newSaveArticleButton = $('<input id = "saveArticle" class="btn btn-primary" value = "Save Article"> </input>').appendTo(newCardBlockDiv);
-
-
+        $(newSaveArticleButton).attr('data-articleId',article._id)
+        $(newSaveArticleButton).attr('data-id',index)
       })
+
+      window.scrollTo(0, $("#about").offset().top);
 
   })
 }
+
+$('#articleRow').on('click','#saveArticle' ,function(){
+
+
+  // Grab the id associated with the article from the submit button
+  var thisId = $(this).attr("data-articleId");
+
+  // Run a POST request to change the note, using what's entered in the inputs
+  $.ajax({
+    method: "GET",
+    url: "/saveArticle/" + thisId
+  })
+    // With that done
+    .done(function(data) {
+      // Log the response
+      console.log(data);
+
+    });
+
+var thisCardId = 'card' + $(this).attr("data-id");
+console.log(thisCardId);
+
+$('#'+thisCardId).fadeOut("slow", function() {
+    // Animation complete.
+});
+})
+
 
 $('#scrapeWSJ').on('click',function() {
 
@@ -48,6 +78,21 @@ $('#scrapeWSJ').on('click',function() {
 
 })
 
+$('#displaySavedArticles').on('click',function() {
+
+  // Now make an ajax call for the Article
+  console.log('Hello');
+
+  $.ajax({
+    method: "GET",
+    url: "/savedArticles"
+  })
+  .done(function(doc) {
+
+    console.log(doc)
+  })
+
+})
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+// Requiring our Note and Article models
+var Note = require(".././models/Note.js");
+var Article = require(".././models/Article.js");
 
 // Get Homepage
 router.get('/', function(req, res){
@@ -13,11 +16,35 @@ res.render('index');
 
 })
 
-router.get('/articles',function(req,res) {
-  Article.find({},function(err,doc) {
-    res.json(doc);
+// Grab an article by it's ObjectId
+router.get("/saveArticle/:id", function(req, res) {
+	console.log('Save Article' + req.params.id)
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  Article.update({ "_id": req.params.id },{saved:true}, function(error,doc) {
+		if (error) {
+      console.log(error);
+    }
+		else {
+      res.json(doc);
+    }
 
-  })
+	})
+
+});
+
+// Grab an article by it's ObjectId
+router.get("/savedArticles", function(req, res) {
+
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  Article.find({ saved:true}, function(error,doc) {
+		if (error) {
+      console.log(error);
+    }
+		else {
+      res.json(doc);
+    }
+
+	})
 
 });
 
